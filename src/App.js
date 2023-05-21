e629dd6268971b9b4291a75e4304f2fced7145ca
aadhar54 <aadhar54@gmail.com>
2023-05-21 17:00:20 +0530

    project 21 May

--- src/App.js
+++ src/App.js
@@ -1,25 +1,29 @@
-import logo from './logo.svg';
-import './App.css';
+// the work of App component is to load all the other custom created components inside the DOM in the sequence that they are specified in the App component
+import { useState } from 'react';
+import Header from './components/Header.jsx';
+import FeedbackData from './data/FeedbackData.js';
+import FeedbackList from './components/FeedbackList.jsx';
+import FeedbackStats from './components/FeedbackStats.jsx';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+function App(){
+    const [feedback, setFeedback] = useState(FeedbackData);
+
+    const deleteFeedback = (id) => {
+        if(window.confirm('Are you sure you want to delete?')){
+            setFeedback(feedback.filter((item) => item.id != id));
+        }
+    }
+    return (
+        <>
+            <Header />
+            <div className="container">
+                <FeedbackStats feedback={feedback}/>
+                <FeedbackList feedback={feedback}
+                handleDelete={deleteFeedback}/>
+            </div>
+        </>
+            
+    )
 }
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -1,17 +1,8 @@
+// the work of index.js is to load the App component inside the div which has id of root.
+
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
-import reportWebVitals from './reportWebVitals';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+ReactDOM.createRoot(document.getElementById('root')).render(<App />)
