38fde70aac85afc2434fa1cd5f170a72ec092af7
aadhar54 <aadhar54@gmail.com>
2023-05-27 17:52:54 +0530

    added feedback form and feedback add functionality

--- src/App.js
+++ src/App.js
@@ -4,6 +4,8 @@ import Header from './components/Header.jsx';
 import FeedbackData from './data/FeedbackData.js';
 import FeedbackList from './components/FeedbackList.jsx';
 import FeedbackStats from './components/FeedbackStats.jsx';
+import FeedbackForm from './components/FeedbackForm.jsx';
+import {v4 as uuidv4} from 'uuid';
 
 function App(){
     const [feedback, setFeedback] = useState(FeedbackData);
@@ -13,10 +15,17 @@ function App(){
             setFeedback(feedback.filter((item) => item.id != id));
         }
     }
+
+    const addFeedback = (newFeedback)=>{
+        newFeedback.id = uuidv4();
+        setFeedback([newFeedback,...feedback]);
+    }
+
     return (
         <>
             <Header />
             <div className="container">
+                <FeedbackForm handleAdd={addFeedback}/>
                 <FeedbackStats feedback={feedback}/>
                 <FeedbackList feedback={feedback}
                 handleDelete={deleteFeedback}/>
